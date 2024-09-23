756bd28995362d05ece8dc9460da0e0b05f269e9
sm kim <k14m29@naver.com>
2024-09-23 13:00:32 +0900

    240923 원데이클래스 리스트에서 모집중/모집마감 출력, 클래스 상세정보 페이지에서 마감일 지나거나 인원 초과된 경우 메시지 출력과 함께 신청 버튼 안보이게 처리

--- furnature/src/main/java/com/example/furnature/dao/OnedayService.java
+++ furnature/src/main/java/com/example/furnature/dao/OnedayService.java
@@ -3,20 +3,28 @@ package com.example.furnature.dao;
 import java.util.HashMap;
 
 public interface OnedayService {
-
+	
+	//원데이클래스 목록 출력
 	HashMap<String,Object> onedayList(HashMap<String,Object> map);
 	
+	//원데이클래스 결제
 	HashMap<String,Object> onedayPay(HashMap<String,Object> map);
 	
+	//원데이클래스 개별 상세 정보
 	HashMap<String,Object> onedayDetail(HashMap<String,Object> map);
 	
+	//원데이클래스 등록(관리자)
 	HashMap<String,Object> onedayReg(HashMap<String,Object> map);
 	
+	//원데이클래스 등록시 파일 업로드
 	HashMap<String,Object> onedayFile(HashMap<String,Object> map);
 	
+	//원데이클래스 등록시 클래스번호 시퀀스로 가져오기
 	HashMap<String,Object> classNo(HashMap<String,Object> map);
 	
+	//원데이클래스 인원초과 여부 확인
 	HashMap<String,Object> numberLimit(HashMap<String,Object> map);
 	
+	//원데이클래스 수정(관리자)
 	HashMap<String,Object> onedayUpdate(HashMap<String,Object> map);
 }	
