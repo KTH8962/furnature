00a20e75da3417319c2f64144edcec7b28c30c33
KTH <gaya8962@gmail.com>
2024-10-04 16:27:00 +0900

    2024 10 04 메인 페이지 작업

--- furnature/src/main/java/com/example/furnature/dao/MainService.java
+++ furnature/src/main/java/com/example/furnature/dao/MainService.java
@@ -6,6 +6,9 @@ public interface MainService {
 	// 상품 리스트 불러오기
 	HashMap<String, Object> searchProductList(HashMap<String, Object> map);
 	
-	// 상품 리스트 불러오기
+	// 원데이클래스 리스트 불러오기
 	HashMap<String, Object> searchOnedayList(HashMap<String, Object> map);
+	
+	// 경매 리스트 불러오기
+	HashMap<String, Object> searchAuctionList(HashMap<String, Object> map);
 }
--- furnature/src/main/java/com/example/furnature/mapper/MainMapper.java
+++ furnature/src/main/java/com/example/furnature/mapper/MainMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.furnature.model.Event;
 import com.example.furnature.model.Oneday;
 import com.example.furnature.model.Product;
 
@@ -15,4 +16,7 @@ public interface MainMapper {
 	
 	// 원데이클래스 리스트 불러오기
 	List<Oneday> selectOnedayList(HashMap<String, Object> map);
+	
+	// 경매 리스트 불러오기
+	List<Event> selectAuctionList(HashMap<String, Object> map);
 }
